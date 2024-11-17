da5ffe9d0d0a9912e11043691dc9c82b40bee006
Sina <santiano38@googlemail.com>
2024-11-17 11:11:01 +0100

    Update main.lua Marker done

--- client/main.lua
+++ client/main.lua
@@ -1,91 +1,101 @@
-Framework = nil
+
+-- Initialize the framework and set initial state
+Framework = getFramework()
 inMenu = false
-sleepWait = true
 cache = {}
 
-if RY.Options.FrameWork == 'esx' then
-    Framework = exports['es_extended']:getSharedObject()   
-elseif RY.Options.FrameWork == 'qb' then
-    Framework = exports['qb-core']:GetCoreObject()
-end
-
-if not RY.Options.oxTarget then
+-- Check if oxTarget option is disabled
+if not (RY.Options.oxTarget) then
     Citizen.CreateThread(function()
         while true do
-            Citizen.Wait(1)
-            sleepWait = true
-            local playerPed = PlayerPedId()
-            local playerCoords = GetEntityCoords(playerPed)
-   
-            if not InMenu then
-                for k,v in pairs(RY.Locations) do
-                    for _, coords in pairs(v.menuCoords) do
-                        local distanceBetwennPlayerAndMenu = #(playerCoords - coords)        
-                        if distanceBetwennPlayerAndMenu < 1 then
-                            sleepWait = false
-                            DrawText3D(coords.x, coords.y, coords.z + 0.25, v.markersConfig.markerMenu.markerText)                        
-                            if IsControlJustReleased(0, v.markersConfig.markerMenu.useKey) then
-                                openMenu(k)
-                            end
-                        end
+            if inMenu then
+                Citizen.Wait(500)
+            else
+                local playerPed = PlayerPedId()
+                local playerCoords = GetEntityCoords(playerPed)
+                local anyNearby = false
 
-                        if distanceBetwennPlayerAndMenu <= 15 then
-                            sleepWait = false
-                            DrawMarker(
-                                v.markersConfig.markerMenu.markerType, 
-                                coords.x, coords.y, coords.z, 
-                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
-                                v.markersConfig.markerMenu.markerSize.x, 
-                                v.markersConfig.markerMenu.markerSize.y, 
-                                v.markersConfig.markerMenu.markerSize.z, 
-                                v.markersConfig.markerMenu.markerColor.r, 
-                                v.markersConfig.markerMenu.markerColor.g, 
-                                v.markersConfig.markerMenu.markerColor.b, 
-                                50, false, true, 2, false, nil, nil, false
-                            )
+                -- Iterate over all shop locations
+                for k, v in pairs(RY.Locations) do
+                    local playerJob = GetPlayerJob()
+                    local canAccess = not v.jobRestrictions.enabled or v.jobRestrictions.jobs[playerJob]
+
+                    if canAccess then
+                        for _, coords in pairs(v.menuCoords) do
+                            local distance = #(playerCoords - coords)
+
+                            -- Check if player is close enough to open the menu
+                            if distance < 1 then
+                                anyNearby = true
+                                if not inMenu then
+                                    -- DrawText3D(coords.x, coords.y, coords.z + 0.25, v.markersConfig.markerMenu.markerText)
+
+                                    -- Open menu if the designated key is released
+                                    if IsControlJustReleased(0, RY.Config.useKey) then
+                                        openMenu(k)
+                                    end
+                                end
+                            end
+
+                            -- Draw marker if player is within 15 units
+                            if distance <= 15 then
+                                anyNearby = true
+                                if not inMenu then
+                                    -- DrawMarker(
+                                    --     v.markersConfig.markerMenu.markerType,
+                                    --     coords.x, coords.y, coords.z,
+                                    --     0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                    --     v.markersConfig.markerMenu.markerSize.x,
+                                    --     v.markersConfig.markerMenu.markerSize.y,
+                                    --     v.markersConfig.markerMenu.markerSize.z,
+                                    --     v.markersConfig.markerMenu.markerColor.r,
+                                    --     v.markersConfig.markerMenu.markerColor.g,
+                                    --     v.markersConfig.markerMenu.markerColor.b,
+                                    --     50, false, true, 2, false, nil, nil, false
+                                    -- )
+                                end
+                            end
                         end
                     end
                 end
-            end
-            if sleepWait then
-                Citizen.Wait(150)
+
+                -- Wait longer if no location is nearby
+                if not anyNearby then
+                    Citizen.Wait(500)
+                else
+                    Citizen.Wait(0)
+                end
             end
         end
     end)
 end
 
+-- Setup targets and blips for each location
 for k, v in pairs(RY.Locations) do
-    if RY.Options.oxTarget then
-        for _, coords in pairs(v.menuCoords) do
-            exports.ox_target:addBoxZone({
-                coords = coords,
-                size = vector3(3,3,3),
-                rotation = 45,
-                debug = false,
-                options = {
-                    {
-                        name = 'shop' .. k,
-                        event = 'ry-shops:openMenu',
-                        args = { location = k },
-                        icon = v.oxTargetConfig.icon,
-                        label = v.oxTargetConfig.label
-                    }
-                }
-            })
-        end
-    end
+    local playerJob = GetPlayerJob()
+    local canAccess = not v.jobRestrictions.enabled or v.jobRestrictions.jobs[playerJob]
 
-    if v.blipsConfig.blipMenu.blipShow then
-        for _, coords in pairs(v.menuCoords) do
-            local shop = AddBlipForCoord(coords.x, coords.y, coords.z)
-            SetBlipSprite(shop, v.blipsConfig.blipMenu.blipSprite)
-            SetBlipDisplay(shop, 4)
-            SetBlipScale(shop, v.blipsConfig.blipMenu.blipScale)
-            SetBlipAsShortRange(shop, true)
-            SetBlipColour(shop, v.blipsConfig.blipMenu.blipColor)
-            BeginTextCommandSetBlipName("STRING")
-            AddTextComponentSubstringPlayerName(v.blipsConfig.blipMenu.blipName)
-            EndTextCommandSetBlipName(shop)
+    if canAccess then
+        -- Setup targets if oxTarget or qbTarget option is enabled
+        if RY.Options.oxTarget or RY.Options.qbTarget then
+            for _, coords in pairs(v.menuCoords) do
+                SetupTargets(k, v, coords)
+            end
+        end
+
+        -- Setup blips if they are configured to be shown
+        if v.blipsConfig.blipMenu.blipShow then
+            for _, coords in pairs(v.menuCoords) do
+                local shop = AddBlipForCoord(coords.x, coords.y, coords.z)
+                SetBlipSprite(shop, v.blipsConfig.blipMenu.blipSprite)
+                SetBlipDisplay(shop, 4)
+                SetBlipScale(shop, v.blipsConfig.blipMenu.blipScale)
+                SetBlipAsShortRange(shop, true)
+                SetBlipColour(shop, v.blipsConfig.blipMenu.blipColor)
+                BeginTextCommandSetBlipName("STRING")
+                AddTextComponentSubstringPlayerName(v.blipsConfig.blipMenu.blipName)
+                EndTextCommandSetBlipName(shop)
+            end
         end
     end
 end
